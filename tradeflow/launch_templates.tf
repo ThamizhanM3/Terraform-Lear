1cc180f9a9b945698cfa32bdd2be34a41af595ee
Thamizhan M3 <mithelesh.tmz.m3@gmail.com>
2026-05-26 12:29:09 +0530

    tradeflow v8

--- tradeflow/launch_templates.tf
+++ tradeflow/launch_templates.tf
@@ -173,16 +173,20 @@ resource "aws_launch_template" "backend_2_launch_template" {
                 #!/bin/bash
 
                 apt update -y
-                apt install -y docker.io
+                apt install -y docker.io nginx
 
                 systemctl start docker
                 systemctl enable docker
+                
+                systemctl start nginx
+                systemctl enable nginx
 
                 docker run -d \
                   --name microservice-3 \
                   --restart unless-stopped \
                   -e PORT=${var.microservice_3_port} \
                   -e MONGODB_URI=mongodb://${aws_instance.tradeflow_database_instance.private_ip}:${var.mongodb_port}/tradeflow \
+                  -e USER_SERVICE_URL="/api \
                   -p ${var.microservice_3_port}:${var.microservice_3_port} \
                   ${var.microservice_3_image}
 
@@ -193,6 +197,77 @@ resource "aws_launch_template" "backend_2_launch_template" {
                   -e MONGODB_URI=mongodb://${aws_instance.tradeflow_database_instance.private_ip}:${var.mongodb_port}/tradeflow \
                   -p ${var.microservice_4_port}:${var.microservice_4_port} \
                   ${var.microservice_4_image}
+
+                cat > /etc/nginx/sites-available/default <<'NGINXCONF'
+                server {
+
+                    listen 80;
+
+                    location / {
+
+                        proxy_pass http://localhost:3000;
+
+                        proxy_http_version 1.1;
+
+                        proxy_set_header Host $host;
+                        proxy_set_header X-Real-IP $remote_addr;
+                        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                        proxy_set_header X-Forwarded-Proto $scheme;
+                    }
+
+                    location /api/users/ {
+
+                        proxy_pass http://${aws_lb.backend_alb.dns_name};
+
+                        proxy_http_version 1.1;
+
+                        proxy_set_header Host $host;
+                        proxy_set_header X-Real-IP $remote_addr;
+                        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                        proxy_set_header X-Forwarded-Proto $scheme;
+                    }
+
+                    location /api/stocks/ {
+
+                        proxy_pass http://${aws_lb.backend_alb.dns_name};
+
+                        proxy_http_version 1.1;
+
+                        proxy_set_header Host $host;
+                        proxy_set_header X-Real-IP $remote_addr;
+                        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                        proxy_set_header X-Forwarded-Proto $scheme;
+                    }
+
+                    location /api/trades/ {
+
+                        proxy_pass http://${aws_lb.backend_alb.dns_name};
+
+                        proxy_http_version 1.1;
+
+                        proxy_set_header Host $host;
+                        proxy_set_header X-Real-IP $remote_addr;
+                        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                        proxy_set_header X-Forwarded-Proto $scheme;
+                    }
+
+                    location /api/portfolio/ {
+
+                        proxy_pass http://${aws_lb.backend_alb.dns_name};
+
+                        proxy_http_version 1.1;
+
+                        proxy_set_header Host $host;
+                        proxy_set_header X-Real-IP $remote_addr;
+                        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                        proxy_set_header X-Forwarded-Proto $scheme;
+                    }
+                }
+                NGINXCONF
+
+                nginx -t
+
+                systemctl restart nginx
                 EOF
     )
 
