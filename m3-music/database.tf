060a841c6a81b5eaec25df76088acf62462bf518
Thamizhan M3 <mithelesh.tmz.m3@gmail.com>
2026-06-09 23:35:22 +0530

    read replica v1

--- m3-music/database.tf
+++ m3-music/database.tf
@@ -48,6 +48,71 @@ resource "aws_instance" "database_instance" {
     }
 }
 
+resource "aws_instance" "mongodb_replica" {
+    ami                    = var.ami_id
+    instance_type          = var.instance_type
+    subnet_id              = aws_subnet.database_subnet_b.id
+    vpc_security_group_ids = [aws_security_group.database_sg.id]
+    key_name               = var.key_name
+    iam_instance_profile   = aws_iam_instance_profile.database_profile.name
+
+    depends_on = [
+        aws_instance.database_instance
+    ]
+
+    user_data = <<-EOF
+                    #!/bin/bash
+
+                    apt update -y
+                    apt install -y docker.io jq
+
+                    systemctl start docker
+                    systemctl enable docker
+
+                    # Start Mongo as replica node
+                    docker volume create mongodb_data
+
+                    docker run -d \
+                    --name mongodb \
+                    --restart unless-stopped \
+                    -p 27017:27017 \
+                    -v mongodb_data:/data/db \
+                    mongo \
+                    --replSet rs0 \
+                    --bind_ip 0.0.0.0
+
+                    sleep 30
+
+                    PRIMARY_IP="${aws_instance.database_instance.private_ip}"
+                    SECONDARY_IP=$(hostname -I | awk '{print $1}')
+
+                    # Convert primary into replica set (if not already)
+                    docker exec mongodb mongosh --host $PRIMARY_IP --eval '
+                    try {
+                        rs.status()
+                    } catch(e) {
+                        rs.initiate({
+                        _id: "rs0",
+                        members: [
+                            { _id: 0, host: "'$PRIMARY_IP':27017" }
+                        ]
+                        })
+                    }
+                    '
+
+                    sleep 15
+
+                    # Add this instance as secondary
+                    docker exec mongodb mongosh --host $PRIMARY_IP --eval "
+                    rs.add('$SECONDARY_IP:27017')
+                    "
+                    EOF
+
+    tags = {
+        Name = "${var.project_name}-Mongo-Replica"
+    }
+}
+
 resource "aws_dynamodb_table" "upload_events" {
     name         = "${var.project_name}-upload-events"
     billing_mode = "PAY_PER_REQUEST"
